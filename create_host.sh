124b3a8d5a6c3bd3ece9ec414846dfe3e0905123
Joe Garcia, CISSP <infamousjoeg@users.noreply.github.com>
2018-02-15 11:58:22 -0500

    Create create_host.sh

---
+++ create_host.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -e
+function main() {
+  echo "-----"
+  fetch_machine_identity
+}
+
+function fetch_machine_identity() {
+
+echo "Hostfactory Token= $(cat /var/jenkins_home/hostfactory)\n"
+echo "Hostname= $(hostname)\n"
+local status="$(curl -k -i -w '%{http_code}' -X POST -H "Authorization: Token token=\"$(cat /var/jenkins_home/hostfactory)\"" https://conjur-master/api/host_factories/hosts?id=$(hostname))"
+
+echo "Status=\n$status\n"
+
+}
+
+main
