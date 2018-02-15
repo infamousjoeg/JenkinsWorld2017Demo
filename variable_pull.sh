c5a32df3e55f607d40efee0b48d640e156aa0aa5
Joe Garcia, CISSP <infamousjoeg@users.noreply.github.com>
2018-02-15 12:04:18 -0500

    Create variable_pull.sh

---
+++ variable_pull.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -e
+function main() {
+  echo "-----"
+  variable_pull
+}
+
+function variable_pull(){
+	local api=$(cat ~/.netrc | grep password | awk '$1=="password"{print $2}')
+	printf "This is the API key = $api\n"
+	local auth_token=$(curl -s -k -H "Content-Type: text/plain" -X POST -d "$api" https://docker/api/authn/users/admin/authenticate)
+	local token=$(echo -n $auth_token | base64 | tr -d '\r\n')
+	printf "\nThe token is: $token\n"
+	local git_username=$(curl -s -k -X GET -H "Authorization: Token token=\"$token\"" https://docker/api/variables/jenkins%2Fgithub%2Fusername/value?)
+	printf "The Variable Value is: $git_username\n"
+}
+
+main
