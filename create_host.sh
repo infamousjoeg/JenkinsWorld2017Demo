8d8b95b32f43f67a951c18b37ddfcd62466b4d7b
Joe Garcia, CISSP <infamousjoeg@users.noreply.github.com>
2018-02-20 15:44:11 -0500

    Update create_host.sh

--- create_host.sh
+++ create_host.sh
@@ -8,7 +8,7 @@ function fetch_machine_identity() {
 
 echo "Hostfactory Token= $(cat /var/jenkins_home/hostfactory)\n"
 echo "Hostname= $(hostname)\n"
-local status="$(curl -k -i -w '%{http_code}' -X POST -H "Authorization: Token token=\"$(cat /var/jenkins_home/hostfactory)\"" https://docker/api/host_factories/hosts?id=$(hostname))"
+local status="$(curl -k -i -w '%{http_code}' -X POST -H "Authorization: Token token=\"$(cat /var/jenkins_home/hostfactory)\"" https://jenkins/api/host_factories/hosts?id=$(hostname))"
 
 echo "Status=\n$status\n"
 
